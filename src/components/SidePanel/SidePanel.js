51d2bd540d08a995001a5f50cdfa9238f5b3bb23
naim71 <naimkhan1314@gmail.com>
2022-09-29 20:22:13 +0600

    design done

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SidePanel = () => {
     return (
-        <div className='mt-6 mx-8'>
+        <div className='mt-6 mx-6'>
             <div className='flex'>
                 <img className="w-12 h-12 rounded-lg mr-5" src="https://sunrift.com/wp-content/uploads/2014/12/Blake-profile-photo-square.jpg" alt="Rounded avatar"></img>
                 <div className='text-left'>
@@ -10,10 +10,53 @@ const SidePanel = () => {
                     <p>Dhaka, Bangladesh</p>
                 </div>
             </div>
-            <div className='bg-slate-200'>
-                
+            <div className='bg-slate-200 flex justify-center	 space-x-14 p-2 py-4 rounded-lg mt-10'>
+                <div>
+                    <p className='text-xl font-bold'>70 kg</p>
+                    <span className='font-semibold text-slate-500'>Weight</span>
+                </div>
+                <div>
+                    <p className='text-xl font-bold'>5.5</p>
+                    <span className='font-semibold text-slate-500'>Height</span>
+                </div>
+                <div>
+                    <p className='text-xl font-bold'>25 yrs</p>
+                    <span className='font-semibold text-slate-500'>Age</span>
+                </div>
             </div>
+
+            <p className='text-xl font-bold text-left mt-10'>Add A Break</p>
+
+            <div className='bg-slate-200 flex justify-center space-x-4 p-2 py-4 rounded-lg mt-10'>
+                <div className='flex items-center bg-white w-15 h-10 rounded-full p-2 font-bold hover:bg-indigo-500 hover:text-white'>
+                   <button>10</button><span>s</span>
+                </div>
+                <div className='flex items-center bg-white w-15 h-10 rounded-full p-2 font-bold hover:bg-indigo-500 hover:text-white'>
+                    <button>20</button><span>s</span>
+                </div >
+                <div className='flex items-center bg-white w-15 h-10 rounded-full p-2 font-bold hover:bg-indigo-500 hover:text-white'>
+                    <button>30</button><span>s</span>
+                </div>
+                <div className='flex items-center bg-white w-15 h-10 rounded-full p-2 font-bold hover:bg-indigo-500 hover:text-white'>
+                    <button>40</button><span>s</span>
+                </div>
+                <div className='flex items-center bg-white w-15 h-10 rounded-full p-2 font-bold hover:bg-indigo-500 hover:text-white'>
+                    <button>50</button><span>s</span>
+                </div>
+            </div>
+
+            <p className='text-xl font-bold text-left mt-10'>Exercise Details</p>
             
+            <div className='bg-slate-200 flex justify-between px-6 py-4 rounded-lg mt-10'>
+                    <p className='text-xl font-bold'>Task Time</p>
+                    <p className='text-xl font-medium'>20<span> minutes</span></p>
+            </div>
+            <div className='bg-slate-200 flex justify-between px-6 py-4 rounded-lg mt-5'>
+                    <p className='text-xl font-bold'>Break Time</p>
+                    <p className='text-xl font-medium'>20<span> minutes</span></p>
+            </div>
+
+            <button className='bg-indigo-500 hover:bg-teal-500 px-20 py-4 rounded mt-14 text-white font-medium'>Activity Completed</button>
 
         </div>
     );
---
+++ src/utilities/RoundButton/RoundButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RoundButton = () => {
+    return (
+        <div className='flex items-center'>
+                    <button>50</button><span>s</span>
+        </div>
+    );
+};
+
+export default RoundButton;
